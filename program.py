7de86233d6710b024b7573dad3c3157a3d399446
Manoj Gautam <nix1947@users.noreply.github.com>
2019-01-21 08:27:59 +0545

    new sum function has been added
    slfjsalkfjslakfjlksajflksafjlkasfjlskafjalskjflskajfaslkfjlsakdfjalskfjlksadjf

--- program.py
+++ program.py
@@ -1,9 +1,20 @@
-
-
-
-
-
-
 print("Hello world")
+
+
+def sum(*a):
+    sum = 0
+  for num in a:
+     sum += num
+  
+  return sum
+
+
+x = sum((1,2,3))
+
+print("Sum of 1,2,3 is", x)
+
+
+
+    
 
 
