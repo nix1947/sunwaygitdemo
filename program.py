091a8d9aba9ffed3d10d2075df46cd64e9610efc
Manoj Gautam <manojit.gautam@gmail.com>
2019-01-21 08:31:02 +0545

    BUG in sum function has been resolved

--- program.py
+++ program.py
@@ -2,14 +2,14 @@ print("Hello world")
 
 
 def sum(*a):
-    sum = 0
+  sum = 0
   for num in a:
      sum += num
   
   return sum
 
 
-x = sum((1,2,3))
+x = sum(1,2,3)
 
 print("Sum of 1,2,3 is", x)
 
